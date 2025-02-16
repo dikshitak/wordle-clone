83784fd3fa791d124bcd0c91587c1b740ab11048
dikshitak <dikshitakasodariya26@gmail.com>
2025-02-16 14:32:05 -0500

    condition added successfully

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,37 +1,53 @@
-import React from 'react'
+import React from "react";
 
-export default function Modal({ isCorrect, solution, turn, showHint, hintHandler }) {
-
-    const hideHintHandler = () => {
-        hintHandler(false);
-    }
-    return (
-        <div className="modal">
-            <div>
-                {showHint && <div className="close-modal" onClick={hideHintHandler}>X</div>}
-                {isCorrect && !showHint && (
-                    <React.Fragment>
-                        <h1>You Win!</h1>
-                        <p className="solution">{solution.word}</p>
-                        <p>You found the solution in {turn} guesses :)</p>
-                    </React.Fragment>
-                )}
-                {!isCorrect && !showHint && (
-                    <React.Fragment>
-                        <h1>Nevermind</h1>
-                        <p className="solution">{solution.word}</p>
-                        <p>Better luck next time :)</p>
-                    </React.Fragment>
-                )}
-                <div className="definition">
-                    <p>Definition</p>
-                    <ul>
-                        {solution.results.map((r,i) => {
-                            return <li key={i}>{r.definition} <span>{`${r.synonyms ? `(Synonyms: ${r.synonyms})` : ''}`}</span></li>
-                        })}
-                    </ul>
-                </div>
-            </div>
+export default function Modal({
+  isCorrect,
+  solution,
+  turn,
+  showHint,
+  hintHandler,
+}) {
+  const hideHintHandler = () => {
+    hintHandler(false);
+  };
+  return (
+    <div className="modal">
+      <div>
+        {showHint && (
+          <div className="close-modal" onClick={hideHintHandler}>
+            X
+          </div>
+        )}
+        {isCorrect && !showHint && (
+          <React.Fragment>
+            <h1>You Win!</h1>
+            <p className="solution">{solution.word}</p>
+            <p>You found the solution in {turn} guesses :)</p>
+          </React.Fragment>
+        )}
+        {!isCorrect && !showHint && (
+          <React.Fragment>
+            <h1>Nevermind</h1>
+            <p className="solution">{solution.word}</p>
+            <p>Better luck next time :)</p>
+          </React.Fragment>
+        )}
+        <div className="definition">
+          <p>Definition</p>
+          <ul>
+            {solution?.results?.map((r, i) => {
+              return (
+                <li key={i}>
+                  {r.definition}{" "}
+                  <span>{`${
+                    r.synonyms ? `(Synonyms: ${r.synonyms})` : ""
+                  }`}</span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-    ) 
+      </div>
+    </div>
+  );
 }
